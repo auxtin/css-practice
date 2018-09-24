3b1900791c0f4287ffbfc09a60a4dd2227332224
IT Express <itexpress@ucmerced-169-236-56-166.ucmerced.edu>
2018-09-24 13:33:18 -0700

    Added more stylings and a footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class footer extends Component {
   render() {
     return (
-        <h1>Footer COMPONENT!!!</h1>
+        <div class='footer'>
+        Austin Leonardo 2018
+        </div>
     )
   }
 }
