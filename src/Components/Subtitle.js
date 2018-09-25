4b9fa6e65c674cfa812f27f36ca6b9abc3bf6cec
auxtin <aleonardo2@ucmerced.edu>
2018-09-25 02:11:16 -0700

    added styling for Clout labels

--- src/Components/Subtitle.js
+++ src/Components/Subtitle.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Subtitle extends Component {
     render() {
         return (
-            <div>
-                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum facilisis accumsan. Curabitur vulputate massa ipsum, eu cursus eros tincidunt.</h4>
+            <div className='subtitle'>
+                <p className='math'>Applied Mathematician</p>
+                <p className='comp'>Computer Scientist</p>
             </div>
         );
     }
