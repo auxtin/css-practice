e88cab2740c6309f1dc05d182d39fa0eb36e26ce
auxtin <aleonardo2@ucmerced.edu>
2018-09-25 01:51:28 -0700

    call me AL stylish

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './Components/header';
 import Footer from './Components/footer';
 import Landing from './Components/Landing';
 import Subtitle from './Components/Subtitle';
+import Lightning from './Components/lightning'
 
 import about from './Components/about';
 import projects from './Components/projects';
@@ -27,9 +28,43 @@ class App extends Component {
           <br></br>
           </div>
           <Subtitle/>
+          <div>
+            <p>
+              <br/>
+              <br/>
+              <br/>
+            </p>
+          </div>
+          <Lightning/>
+          <div>
+            <p>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+            </p>
+          </div>
+          <section id='about'>
+            <h2>About Austin....</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id enim justo. Integer ullamcorper eget mi sit amet ultricies. Proin bibendum nunc vitae felis hendrerit, eu auctor arcu auctor. Pellentesque pretium justo leo, et sagittis justo auctor et. Etiam cursus leo ut porta cursus. Phasellus mollis risus non justo euismod, sit amet imperdiet leo sodales. Sed efficitur, turpis ornare consequat rhoncus, enim ante mattis lectus, ut interdum dolor enim sit amet arcu. Nulla id tortor nec lacus accumsan hendrerit at vitae tortor. Etiam at pretium nunc, ac rhoncus arcu. Praesent ligula sem, porttitor rutrum mattis et, eleifend eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+          </section>
           {/* <Route path='/about' component={about} />
           <Route path='/projects' component={projects} /> */}
-          <Footer />
+          <Footer/>
         </div>
       </Router>
     );
--- src/Components/lightning.js
+++ src/Components/lightning.js
@@ -4,7 +4,7 @@ class lightning extends Component {
     render() {
         return (
             <div>
-                <img src='/Assets/lightning.jpg'/>
+                <a href='#about'><img src='https://png2.kisspng.com/sh/aa6f14bcffd3015f8d1e20ffefd7420c/L0KzQYm3U8IxN6pwj5H0aYP2gLBuTfxqb5l5htt3Zz3lfMbsTfNtcaEyeeR9LYDrf8X2Tfxqb5l5htt3Zz3lf737TgBvb146edQ6OEe2dYSBhsdjO183TKY7OUC1RIK8UsE2QGQAS6Y7M0O2PsH1h5==/kisspng-lightning-blue-clip-art-photo-lightning-bolt-png-5ab1873e38f7b3.2442902415215839342333.png' className='lightning'/></a>
             </div>
         );
     }
